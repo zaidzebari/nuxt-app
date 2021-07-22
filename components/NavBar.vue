eec96b5a19de6a797d882112f094f8f7a08cf7fe
Zaid Salah <zaidsalah351@gmail.com>
2021-07-22 11:57:01 +0300

    added navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="
+      flex flex-row
+      justify-between
+      items-center
+      pl-4
+      py-2
+      bg-indigo-700
+      text-white
+      sticky
+      top-0
+    "
+  >
+  <nuxt-link to="/" class="text-4xl font-bold cursor-pointer text-white">Nuxt Post</nuxt-link>
+
+    <!-- <div class="text-4xl font-bold cursor-pointer">Nuxt Post</div> -->
+    <div>
+      <ul class="space-x-3 flex items-center pr-3">
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/users">Users</nuxt-link></li>
+        <li><nuxt-link to="/posts">Posts</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li><nuxt-link to="/about">Login</nuxt-link></li>
+        <li><nuxt-link to="/about">Register</nuxt-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+a.nuxt-link-active:last-child {
+  font-weight: bold;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active:last-child {
+  color: #00c58e;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-indigo-200">
+    <nav-bar/>
+    <div class="bg-indigo-100 px-8 min-h-screen min-w-screen">
+      <Nuxt />
+    </div>
+    <div class="flex justify-center bg-indigo-400 text-white py-3">
+      here is footer
+    </div>
+  </div>
+</template>
+<script>
+// import MyNav from "../components/Nav" 
+// export default {
+//   components:{
+//     MyNav
+//   }
+  
+// }
+</script>
