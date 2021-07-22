c2c14c55a9b310a9bfc21b07620c610259b641e1
Zaid Salah <zaidsalah351@gmail.com>
2021-07-22 11:28:57 +0300

    init nuxt app with tailwind css

---
+++ components/Home.vue
@@ -0,0 +1,6 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 text-4xl text-black font-bold">
+    Zaid | Full-Stack Devloper
+  </div>
+</template>
